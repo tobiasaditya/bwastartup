b9e41eb1e2b09ee32ba11a41b347d4d94db00da7
Tobias <tobias.aditya@gmail.com>
2022-05-15 15:58:03 +0700

    add entity and repository campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,8 @@
 package handler
+
+//Fitur
+// 1. Create campaign
+// 2.List campaign (filter by user, atau kalau no filter, tampilin semua)
+//	tangkap input filter
+//  ke service, menentukan apakah dapetin semua campaign atau filter by user_id
+// repository akses ke db
