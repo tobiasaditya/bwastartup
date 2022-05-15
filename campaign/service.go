5499b36a15556f46b9960722a649dc1bbd0c3e40
Tobias <tobias.aditya@gmail.com>
2022-05-15 16:10:26 +0700

    add campaign service

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,29 @@
 package campaign
+
+type Service interface {
+	FindCampaigns(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository: repository}
+}
+
+func (s *service) FindCampaigns(userID int) ([]Campaign, error) {
+	if userID == 0 {
+		foundCampaigns, err := s.repository.FindAll()
+		if err != nil {
+			return foundCampaigns, err
+		}
+		return foundCampaigns, nil
+	} else {
+		foundCampaigns, err := s.repository.FindByUserID(userID)
+		if err != nil {
+			return foundCampaigns, err
+		}
+		return foundCampaigns, nil
+	}
+}
